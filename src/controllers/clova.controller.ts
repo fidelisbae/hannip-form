8790e96f493cfc61ac4d4ce2c4384c4959838638
fidelisbae <fidelisbae@gmail.com>
2024-07-23 00:09:06 +0900

    feat: clova api

--- src/controllers/clova.controller.ts
+++ src/controllers/clova.controller.ts
@@ -7,15 +7,14 @@ export async function createIdea(
   next: NextFunction,
 ) {
   try {
-    const { subject, essential, opening, closing, length, tone } = req.body;
+    const { category, info, content } = req.body;
 
     const url =
       'https://clovastudio.stream.ntruss.com/testapp/v1/chat-completions/HCX-003';
     const headers = {
       'X-NCP-CLOVASTUDIO-API-KEY':
         'NTA0MjU2MWZlZTcxNDJiY/IIs6hKNWu7jVWk9J1uJCZ9ajHs0keWuO7NCEIcJWPp',
-      'X-NCP-APIGW-API-KEY': 'LuUo3tEvQF1nnnZA4vOWmuJghmGhJvCCpUTsXG2E',
-      //   'X-NCP-CLOVASTUDIO-REQUEST-ID': '736e4666-433c-45a4-8cc7-5414ac888df0',
+      'X-NCP-APIGW-API-KEY': process.env.NCP_API_GATEWAY_KEY,
       'Content-Type': 'application/json',
     };
 
@@ -23,18 +22,25 @@ export async function createIdea(
       messages: [
         {
           role: 'system',
-          content: `1. 답변의 말투는 ${tone} 로 해주세요.
-                    2. 답변은 한가지만 제공합니다.`,
+          content: `1. 답변은 배열형태로 다섯가지를 제공해야 합니다.
+                    2. 설명은 생략하고 결과만 제공해야 합니다.
+                    3. 명사 형태로 제공해야 합니다.`,
         },
         {
           role: 'user',
-          content: `숏폼을 제작할건데 아래 조건에 맞는 아이디어를 추천해줘
-          주제: ${subject}
-          필수포함내용: ${essential}
-          첫인사: ${opening}
-          맺음말: ${closing}
-          영상길이: ${length}
-          `,
+          content: `파리올림픽 관련으로 숏폼을 만들건데 아이디어 생성해줘`,
+        },
+        {
+          role: 'assistant',
+          content: `[파리 올림픽 종목 체험기, 파리 올림픽 선수들과의 인터뷰, 파리 올림픽 응원가 제작하기, 파리 올림픽 마스코트와 함께하는 댄스 챌린지, 파리 올림픽 경기장 및 주변 관광지 소개]`,
+        },
+        {
+          role: 'user',
+          content: `숏폼을 만들건데 아이디어 생성해줘
+
+                    제작하고 싶은 숏폼 영상 카테고리: ${category}
+                    내 채널 소개: ${info}
+                    만들고 싶은 내용: ${content}`,
         },
       ],
       topP: 0.8,
@@ -49,7 +55,65 @@ export async function createIdea(
 
     const response = await axios.post(url, data, { headers });
 
-    const result = response.data;
+    const clovaResult = response.data;
+    const ideasString = clovaResult.result.message.content;
+    const result = ideasString.slice(1, -1).split(', ');
+
+    return res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function createScript(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const { idea, essential, opening, closing, length, tone } = req.body;
+
+    const url =
+      'https://clovastudio.stream.ntruss.com/testapp/v1/chat-completions/HCX-003';
+    const headers = {
+      'X-NCP-CLOVASTUDIO-API-KEY':
+        'NTA0MjU2MWZlZTcxNDJiY/IIs6hKNWu7jVWk9J1uJCZ9ajHs0keWuO7NCEIcJWPp',
+      'X-NCP-APIGW-API-KEY': process.env.NCP_API_GATEWAY_KEY,
+      'Content-Type': 'application/json',
+    };
+
+    const data = {
+      messages: [
+        {
+          role: 'system',
+          content: `1. 답변은 문자열 형태로 한가지를 제공해야 합니다.
+                    2. 설명은 생략하고 결과만 제공해야 합니다.
+                    3. ${tone} 말투로 제공해야 합니다.`,
+        },
+        {
+          role: 'user',
+          content: `해당 내용으로 숏폼 대본을 만들어줘
+                    주제: ${idea}
+                    필수 요소: ${essential}
+                    첫인사: ${opening}
+                    끝맺음: ${closing}
+                    영상 길이: ${length}`,
+        },
+      ],
+      topP: 0.8,
+      topK: 0,
+      maxTokens: 256,
+      temperature: 0.5,
+      repeatPenalty: 5.0,
+      stopBefore: [],
+      includeAiFilters: true,
+      seed: 0,
+    };
+
+    const response = await axios.post(url, data, { headers });
+
+    const clovaResult = response.data;
+    const result = clovaResult.result.message.content;
 
     return res.status(200).json(result);
   } catch (error) {
--- src/routers/clova.router.ts
+++ src/routers/clova.router.ts
@@ -5,5 +5,6 @@ import * as controller from '../controllers/clova.controller';
 const router = express.Router();
 
 router.route('/idea').post(controller.createIdea);
+router.route('/script').post(controller.createScript);
 
 export default router;
